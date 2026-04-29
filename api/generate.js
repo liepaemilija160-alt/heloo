a9f99ae1322c63d1654e39ca769e75973071a5fa
liepaemilija160-alt <liepaemilija160@gmail.com>
2026-04-29 02:11:02 +0100

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,5 +1,5 @@
-export default async function handler(req, res) {
-  return res.status(200).json({
-    image: "https://via.placeholder.com/300"
+export default function handler(req, res) {
+  res.status(200).json({
+    image: "https://picsum.photos/400"
   });
 }
