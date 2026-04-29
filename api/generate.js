f51da02a9d88f3cc33253d6cbc95e802dd14486b
liepaemilija160-alt <liepaemilija160@gmail.com>
2026-04-29 02:29:28 +0100

    improve image generator

--- api/generate.js
+++ api/generate.js
@@ -1,5 +1,17 @@
 export default function handler(req, res) {
-  res.status(200).json({
-    image: "https://picsum.photos/400"
-  });
+  const { prompt } = req.body;
+
+  let image = "";
+
+  if (prompt.toLowerCase().includes("car")) {
+    image = "https://images.unsplash.com/photo-1503376780353-7e6692767b70";
+  } 
+  else if (prompt.toLowerCase().includes("cat")) {
+    image = "https://images.unsplash.com/photo-1518791841217-8f162f1e1131";
+  } 
+  else {
+    image = "https://picsum.photos/400";
+  }
+
+  res.status(200).json({ image });
 }
